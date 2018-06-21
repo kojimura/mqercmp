af01bb254a133a7f712bc8efc88820dbc0c986c9
kojimura <33794193+kojimura@users.noreply.github.com>
2018-06-22 05:52:22 +0900

    Update mqercmp.ps1

--- ps/mqercmp.ps1
+++ ps/mqercmp.ps1
@@ -1,36 +1,34 @@
-<#
-Simplify all MQ FDC files in spesified directory
-usage: .\mqercmp.ps1 C:\home\myprog\mqercmp\testdata
-#>
 Param( $p1 )
 $f = Get-ChildItem $p1 -Filter *.LOG
 foreach ($i in $f){
  $flag=0;
  echo $i.Name
+ $out=$i.Name.Split('.')[0]
+ $out=$out.Substring($out.Length - 1,1)+".txt"
+ echo $out
  cat $i.FullName | %{
  if($_ -match "^(\d+)/(\d+)/(\d+)" -or $_ -match "^(\d+)年(\d+)月(\d+)日"){
-   $d1=$_.Split(' ')[0];
-   $d2=$_.Split(' ')[1];
-   $flag=1;
+   $date=$_.Split(' ')[0]
+   $time=$_.Split(' ')[1]
+   $flag=1
   }
   elseif($_.StartsWith("AMQ")){
-   $d3=$_;
-   Write-Output -NoEnumerate "$d1 $d2 $d3";
+   $err=$_
+   $buf=$date+' '+$time+' '+$err
    if($_.EndsWith("。") -or $_.EndsWith(".")){
-<#    Write-Output ""; #>
+    Write-Output "$buf"
     $flag=0;
    }
    else{
-    $flag=2;
+    $flag=2
    }
   }
   elseif($flag -eq 2){
-   Write-Output -NoEnumerate $_;
+   $buf+=$_;
    if($_.EndsWith("。") -or $_.EndsWith(".")){
-<#    Write-Output "";#>
-    $flag=0;
+    Write-Output "$buf"
+    $flag=0
    }
   }
  }
 }
-<# buf末尾に追加すべき #>
