47ba682a8c19864623ca4504770f2a6e02a4f719
Kouji Murakami <kojimura@gmail.com>
2018-07-06 13:33:13 +0900

    remove \r

--- sh/mqer.sh
+++ sh/mqer.sh
@@ -3,9 +3,10 @@
 # Simplify all MQ error files in spesified directory
 # usage: mqer.sh /var/mqm/errors/
 
-echo $0 $1 $2
-ScriptDir=$(cd $(dirname $0); pwd)
-echo $ScriptDir
+#echo $0 $1 $2
+#ScriptDir=$(cd $(dirname $0); pwd)
+ScriptDir="/usr/local/src"
+#echo $ScriptDir
 
 fPath=$1
 for fname in ${fPath}*.LOG; do
